446d1926db967519b9691b3f60031aa7bfe1bd74
Jacob <j3kstrum@gmail.com>
2021-10-30 19:08:27 -0700

    Add ability to wrap text and retrieve a (possibly infinitely long) text image. Adds description panel in a clean way and prepares for eventual description scrolling.

--- src/main/java/com/runeliteminigame/util/ImageUtils.java
+++ src/main/java/com/runeliteminigame/util/ImageUtils.java
@@ -6,14 +6,15 @@ import net.runelite.client.ui.FontManager;
 import javax.imageio.ImageIO;
 import java.awt.Font;
 import java.awt.FontMetrics;
+import java.awt.Graphics;
 import java.awt.Point;
-import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Class containing static methods designed to simplify image-related transformation and utility functions.
@@ -97,6 +98,82 @@ public class ImageUtils {
                 return EMPTY_IMAGE;
             }
         }
+    }
+
+    /**
+     * Generates an image, with word wrap, using the selected text.
+     * The output image will have a fixed width and will have an output height depending on the amount of text rendered.
+     * @param textToRender The text to render on the image.
+     * @param graphics The graphics object, containing the font used to render the text.
+     * @param fixedWidth The width of the output image. The height will be variable.
+     * @return The output image with variable height, consisting of the provided text.
+     */
+    public static BufferedImage getTextImageScrollVertical(String textToRender, Graphics graphics, int fixedWidth) {
+        ArrayList<String> textRows = ImageUtils.wrappedText(textToRender, graphics, fixedWidth, textToRender.length());
+        if (textRows.size() == 0) {
+            return new BufferedImage(0, 0, BufferedImage.TYPE_INT_ARGB);
+        }
+        int sizePerRow = (int)Math.ceil(graphics.getFontMetrics().getStringBounds(textRows.get(0), graphics).getHeight());
+        BufferedImage outputImage = new BufferedImage(fixedWidth, sizePerRow * textRows.size(), BufferedImage.TYPE_INT_ARGB);
+        int offset = sizePerRow;
+        outputImage.getGraphics().setFont(graphics.getFont());
+        for(String line : textRows) {
+            outputImage.getGraphics().drawString(line, 0, offset);
+            offset += sizePerRow;
+        }
+        return outputImage;
+    }
+
+    private static ArrayList<String> wrappedText(String longText, Graphics graphics, int maximumWidth, int suggestedLength) {
+        // Bounded search for the number of characters than can be rendered.
+        // Find max length from the start of the string
+        if (longText.isEmpty()) {
+            return new ArrayList<>();
+        }
+        int max_chars = longText.length();
+        int min_chars = 1;
+        int current = Math.min(suggestedLength, longText.length());
+        while (max_chars != min_chars) {
+            Rectangle2D bounds = graphics.getFontMetrics().getStringBounds(longText.substring(0, current), graphics);
+            int stringWidth = (int)Math.ceil(bounds.getWidth());
+            if (stringWidth > maximumWidth) {
+                // Must choose a smaller string.
+                max_chars = current - 1;
+                // Linear interpolation for next string size.
+                float charsPerPixel = (float)current / stringWidth;
+                current = Math.round(charsPerPixel * maximumWidth);
+                // Make sure that we converge and stay within bounds.
+                current = Math.max(Math.min(current, max_chars), min_chars);
+            }
+            else if (stringWidth == maximumWidth) {
+                // Exact match.
+                break;
+            }
+            else {
+                // Should look for a larger string, where possible. But this one works too.
+                min_chars = current;
+                // Linear interpolation for next string size.
+                float charsPerPixel = (float)current / stringWidth;
+                int guess = Math.round(charsPerPixel * maximumWidth);
+                // Make sure that we converge and stay within bounds.
+                current = Math.min(max_chars, Math.max(current + 1, guess));
+            }
+        }
+
+        String substring = longText.substring(0, current).trim();
+        int adjustedIndex = current;
+        if (substring.contains(" ")) {
+            // We'll shrink the substring to prevent cutting off words halfway.
+            int spaceLocation = substring.lastIndexOf(" ");
+            adjustedIndex = spaceLocation + 1;
+            substring = longText.substring(0, adjustedIndex).trim();
+        }
+        String remaining = longText.substring(adjustedIndex);
+
+        ArrayList<String> result = new ArrayList<>();
+        result.add(substring);
+        result.addAll(ImageUtils.wrappedText(remaining, graphics, maximumWidth, current));
+        return result;
     }
 
 }
